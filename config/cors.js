242977d9f6cc253f4548b1c75fc6d947907a86fc
jheart-vic <adebowalevictorjheart@gmail.com>
2026-01-16 20:54:02 +0100

    add patch to cors methods

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   exposedHeaders: ['Authorization', 'refresh_token'],
