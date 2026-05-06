e7834bf8a55bf76722bfabb01c66832a26358dcf
jheart-vic <adebowalevictorjheart@gmail.com>
2026-05-06 08:28:59 +0100

    deduct from wallet (Admin ) fixed

--- models/updateFund.model.js
+++ models/updateFund.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const updateFundSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     amount: { type: Number, required: true },
     message: { type: String },
     type: { type: String, enum: ["credit", "debit"], required: true },
